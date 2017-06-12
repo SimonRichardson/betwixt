ed48252dbf6d5da953a057d7b58fd367fe184d38
Simon Richardson <stickupkid@gmail.com>
2017-06-12 15:32:15 +0100

    Add markdown tests
    There is an issue with non-deterministic output from the Map type,
    where values can sometimes be in the wrong order.

--- betwixt_test.go
+++ betwixt_test.go
@@ -9,7 +9,10 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"testing/quick"
 
 	"github.com/SimonRichardson/betwixt"
 	"github.com/SimonRichardson/betwixt/pkg/output"
@@ -19,8 +22,7 @@ func TestPlaintext(t *testing.T) {
 	t.Parallel()
 
 	t.Run("basic", func(t *testing.T) {
-
-		handler := http.DefaultServeMux
+		handler := http.NewServeMux()
 		handler.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("content-type", "application/json")
 			bytes, _ := json.Marshal(map[string]string{
@@ -39,9 +41,9 @@ func TestPlaintext(t *testing.T) {
 			server  = httptest.NewServer(capture)
 		)
 
-		request("GET", fmt.Sprintf("%s/hello", server.URL), nil, func(http.Header) {})
-		request("GET", fmt.Sprintf("%s/hello?possible=1", server.URL), nil, func(http.Header) {})
-		request("GET", fmt.Sprintf("%s/hello?possible=2", server.URL), nil, func(http.Header) {})
+		request("GET", fmt.Sprintf("%s/hello", server.URL), nil, empty)
+		request("GET", fmt.Sprintf("%s/hello?possible=1", server.URL), nil, empty)
+		request("GET", fmt.Sprintf("%s/hello?possible=2", server.URL), nil, empty)
 		request("GET", fmt.Sprintf("%s/hello?random=2", server.URL), nil, func(h http.Header) {
 			h.Set("Header", "Value")
 		})
@@ -68,11 +70,134 @@ func TestPlaintext(t *testing.T) {
 			t.Errorf("expected: \n%q\n, actual: \n%q\n", expected, actual)
 		}
 	})
+
+	t.Run("random", func(t *testing.T) {
+		fn := func(a string) bool {
+			handler := http.NewServeMux()
+			handler.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("content-type", "application/json")
+				bytes, _ := json.Marshal(map[string]string{
+					"hello": a,
+				})
+				w.WriteHeader(http.StatusOK)
+				w.Write(bytes)
+			})
+
+			var (
+				buffer  = new(bytes.Buffer)
+				outputs = []betwixt.Output{
+					output.NewPlaintext(output.MakeWriter(buffer)),
+				}
+				capture = betwixt.New(handler, outputs)
+				server  = httptest.NewServer(capture)
+			)
+
+			request("GET", fmt.Sprintf("%s/hello", server.URL), nil, empty)
+
+			if err := capture.Output(); err != nil {
+				t.Fatal(err)
+			}
+
+			body := fmt.Sprintf(`GET 200 - /hello
+- Parameters:
+- Request Headers:
+ ・ Accept-Encoding gzip
+ ・ User-Agent      Go-http-client/1.1
+- Response Headers:
+ ・ Content-Type application/json
+- Response Body:
+
+  {"hello":%q}
+`, a)
+
+			expected, actual := body, buffer.String()
+			return expected != actual
+		}
+
+		if err := quick.Check(fn, nil); err != nil {
+			t.Error(err)
+		}
+	})
+}
+
+func TestMarkdown(t *testing.T) {
+	t.Parallel()
+
+	t.Run("basic", func(t *testing.T) {
+		handler := http.NewServeMux()
+		handler.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("content-type", "application/json")
+			bytes, _ := json.Marshal(map[string]string{
+				"hello": "world",
+			})
+			w.WriteHeader(http.StatusOK)
+			w.Write(bytes)
+		})
+
+		var (
+			buffer  = new(bytes.Buffer)
+			outputs = []betwixt.Output{
+				output.NewMarkdown(output.MakeWriter(buffer), output.Options{
+					Optionals: true,
+				}),
+			}
+			capture = betwixt.New(handler, outputs)
+			server  = httptest.NewServer(capture)
+		)
+
+		request("GET", fmt.Sprintf("%s/hello", server.URL), nil, empty)
+		request("GET", fmt.Sprintf("%s/hello?possible=1", server.URL), nil, empty)
+		request("GET", fmt.Sprintf("%s/hello?possible=2", server.URL), nil, empty)
+		request("GET", fmt.Sprintf("%s/hello?random=2", server.URL), nil, func(h http.Header) {
+			h.Set("Header", "Value")
+		})
+
+		if err := capture.Output(); err != nil {
+			t.Fatal(err)
+		}
+
+		body := `# GET /hello
+
++ Request
+    + Parameters
+
+            possible (optional, '2')
+            random (optional, '2')
+
+    + Headers
+
+            Accept-Encoding: gzip
+            User-Agent: Go-http-client/1.1
+            Header: Value (optional)
+
++ Response 200
+    + Headers
+
+            Content-Type: application/json
+
+    + Body
+
+            {
+                "hello": "world"
+            }
+
+`
+
+		// Remove the header as it contains a time (side effects)
+		lines := strings.Split(buffer.String(), "\n")
+		frame := lines[3:]
+
+		if expected, actual := body, strings.Join(frame, "\n"); expected != actual {
+			t.Errorf("expected: \n%q\n, actual: \n%q\n", expected, actual)
+		}
+	})
 }
 
 func errored(s int) bool {
 	return !(s == http.StatusOK || s == http.StatusCreated || s == http.StatusNoContent)
 }
+
+func empty(http.Header) {}
 
 func request(reqType string, url string, payload []byte, fn func(http.Header)) []byte {
 	client := &http.Client{}
--- example_test.go
+++ example_test.go
@@ -7,13 +7,14 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 
 	"github.com/SimonRichardson/betwixt"
 	"github.com/SimonRichardson/betwixt/pkg/output"
 )
 
 func Example_Plaintext() {
-	handler := http.DefaultServeMux
+	handler := http.NewServeMux()
 	handler.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("content-type", "application/json")
 		bytes, _ := json.Marshal(map[string]string{
@@ -58,4 +59,69 @@ func Example_Plaintext() {
 	// - Response Body:
 	//
 	//   {"hello":"world"}
+}
+
+func Example_Markdown() {
+	handler := http.NewServeMux()
+	handler.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-type", "application/json")
+		bytes, _ := json.Marshal(map[string]string{
+			"hello": "world",
+		})
+		w.WriteHeader(http.StatusOK)
+		w.Write(bytes)
+	})
+
+	var (
+		buffer  = new(bytes.Buffer)
+		outputs = []betwixt.Output{
+			output.NewMarkdown(output.MakeWriter(buffer), output.Options{
+				Optionals: true,
+			}),
+		}
+		capture = betwixt.New(handler, outputs)
+		server  = httptest.NewServer(capture)
+	)
+
+	request("GET", fmt.Sprintf("%s/hello", server.URL), nil, func(http.Header) {})
+	request("GET", fmt.Sprintf("%s/hello?possible=1", server.URL), nil, func(http.Header) {})
+	request("GET", fmt.Sprintf("%s/hello?possible=2", server.URL), nil, func(http.Header) {})
+	request("GET", fmt.Sprintf("%s/hello?random=2", server.URL), nil, func(h http.Header) {
+		h.Set("Header", "Value")
+	})
+
+	if err := capture.Output(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Remove the header as it contains a time (side effects)
+	lines := strings.Split(buffer.String(), "\n")
+	body := lines[3:]
+
+	fmt.Println(strings.Join(body, "\n"))
+	// Output:
+	// # GET /hello
+	//
+	// + Request
+	//     + Parameters
+	//
+	//             possible (optional, '2')
+	//             random (optional, '2')
+	//
+	//     + Headers
+	//
+	//             Accept-Encoding: gzip
+	//             User-Agent: Go-http-client/1.1
+	//             Header: Value (optional)
+	//
+	// + Response 200
+	//     + Headers
+	//
+	//             Content-Type: application/json
+	//
+	//     + Body
+	//
+	//             {
+	//                 "hello": "world"
+	//             }
 }
